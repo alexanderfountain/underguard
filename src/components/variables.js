91e9243903688be353f0a91c0d3879e719e3d55f
Alexander Fountain <alexander@digett.com>
2019-02-12 17:47:40 -0500

    coming soon static

--- src/components/variables.js
+++
@@ -1,8 +0,0 @@
-export const desktopWidth = '1200px'
-export const tabletWidth = '1000px'
-export const mobileWidth = '800px'
-export const darkGray = '#232525'
-export const gray = '#5b5e5e'
-export const lightGray = '#eeeeee'
-export const red = '#e85555'
-export const brand1 = '#6bc000'
