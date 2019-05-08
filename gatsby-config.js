0652dd0e2720c3978b50c2520a74b9590ea6d306
Alexander Fountain <alexander@digett.com>
2019-05-07 19:48:17 -0500

    change build to yarn

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,13 +37,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/static/assets`,
-        name: `assets`
-      }
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
