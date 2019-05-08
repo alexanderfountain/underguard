ddac10f5aa40d1679f20b8e81aca56cb2e4909e1
Alexander Fountain <alexander@digett.com>
2019-05-07 19:14:36 -0500

    contact form fix

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -19,6 +19,30 @@ flex-wrap:wrap;
 justify-content:space-between;
 padding:50px 0px;
 border-bottom: thin solid #eee;
+button{
+  color: white;
+  background-color:black;
+  border-radius: 25px;
+  padding: 10px 30px;
+  border: 0px;
+  color: white;
+  font-size:22px;
+  display: inline-block;
+  cursor:pointer;
+}
+label{
+  font-weight:bold;
+}
+input{
+  padding:20px 20px;
+  border-radius:30px
+}
+textarea{
+  padding:20px 30px
+  text-align:left;
+  min-height:100px;
+  border-radius:10px;
+}
 @media (max-width: ${variable.mobileWidth}) {
 
 }
@@ -86,29 +110,27 @@ export default class ContactPage extends React.Component {
         </ContactLeft>
       <ContactRight>
       <Form>
-      <form name="contact" method="post" netlify-honeypot="bot-field" data-netlify="true">
-			<input type="hidden" name="form-name" value="contact" />
+      <form name="contact" method="post" action="/thank-you" netlify-honeypot="bot-field" data-netlify="true">
+			<input type="hidden" name="form-name" value="homecontact" />
 			<p hidden> <label htmlFor="bot-field">Don’t fill this out:{' '}<input name="bot-field" /> </label> </p>
 								<div class="form-group">
-									<input type="text" placeholder="Name" name="name" id="name" class="form-control" data-required="true" data-interactive="true" />
+									<label for="name" class="lb-name">NAME</label>
+									<input type="text" name="name" id="name" class="form-control" data-required="true" data-interactive="true" required />
 								</div>
 								<div class="form-group">
-									<input type="email" name="email" placeholder="Email" id="email" class="form-control" data-required="true" data-interactive="true" />
+									<label for="email" class="lb-email">EMAIl ADDRESS</label>
+									<input type="email" name="email" id="email" class="form-control" data-required="true" data-interactive="true" required />
 								</div>
 								<div class="form-group">
-									<input type="tel" name="phone" id="phone" placeholder="Phone Number" class="form-control" data-required="false" data-interactive="true" />
+									<label for="phone" class="lb-phone">PHONE</label>
+									<input type="tel" name="phone" id="phone" class="form-control" data-required="false" data-interactive="true" required />
 								</div>
 								<div class="form-group text">
-									<textarea name="textarea" id="textarea" placeholder="Message" class="textarea form-control" data-required="true" data-trim="true"/>
+									<label for="textarea" class="lb-message">YOUR MESSAGE</label>
+									<textarea name="textarea" id="textarea" class="textarea form-control" data-required="true" data-trim="true"/>
 								</div>
 								<div>
-									{/* <Styledbutton type="submit" style={{
-                    width:'100%',
-                    marginTop:'0px',
-
-                  }}>
-                  Contact
-                  </Styledbutton> */}
+									<button type="submit" class="btn btn-submit">SEND MESSAGE</button>
 								</div>
 							</form>
           </Form>
