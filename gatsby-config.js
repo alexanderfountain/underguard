015b2aff34a6f80aef76de96f964df562ada26ba
Alexander Fountain <alexander@digett.com>
2019-05-07 19:52:20 -0500

    gatsby build

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,13 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        path: `${__dirname}/static/assets`,
+        name: `assets`
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
