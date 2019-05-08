d35cbd31e6569bd35ab1e779975f26354ec37a5c
Alexander Fountain <alexander@digett.com>
2019-05-07 19:10:53 -0500

    required form fields

--- src/components/organisms/sections/section-form.js
+++ src/components/organisms/sections/section-form.js
@@ -44,15 +44,15 @@ const SectionForm = ({object}) => {
 			<p hidden> <label htmlFor="bot-field">Don’t fill this out:{' '}<input name="bot-field" /> </label> </p>
 								<div class="form-group">
 									<label for="name" class="lb-name">NAME</label>
-									<input type="text" name="name" id="name" class="form-control" data-required="true" data-interactive="true" />
+									<input type="text" name="name" id="name" class="form-control" data-required="true" data-interactive="true" required />
 								</div>
 								<div class="form-group">
 									<label for="email" class="lb-email">EMAIl ADDRESS</label>
-									<input type="email" name="email" id="email" class="form-control" data-required="true" data-interactive="true" />
+									<input type="email" name="email" id="email" class="form-control" data-required="true" data-interactive="true" required />
 								</div>
 								<div class="form-group">
 									<label for="phone" class="lb-phone">PHONE</label>
-									<input type="tel" name="phone" id="phone" class="form-control" data-required="false" data-interactive="true" />
+									<input type="tel" name="phone" id="phone" class="form-control" data-required="false" data-interactive="true" required />
 								</div>
 								<div class="form-group text">
 									<label for="textarea" class="lb-message">YOUR MESSAGE</label>
