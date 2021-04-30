0ba4292c0b9d3b2e5c7b43a2c5246fd9c7462385
Alexander Fountain <fountain@AlexandersMBP2.PK5001Z>
2021-04-29 22:31:59 -0500

    podcast comment out

--- src/pages/podcasts.js
+++ src/pages/podcasts.js
@@ -1,146 +1,146 @@
-import React, { useState, useEffect } from 'react'
-import { Link, graphql } from 'gatsby'
-import Helmet from 'react-helmet'
-import Layout from '../components/layout/layout'
-import Container from '../components/layout/container'
-import styled from 'styled-components'
-import { Howl, Howler } from 'howler'
+// import React, { useState, useEffect } from 'react'
+// import { Link, graphql } from 'gatsby'
+// import Helmet from 'react-helmet'
+// import Layout from '../components/layout/layout'
+// import Container from '../components/layout/container'
+// import styled from 'styled-components'
+// import { Howl, Howler } from 'howler'
 
-const PlayPause = ({ isPlaying, onClick }) =>
-  isPlaying ? (
-    <img
-      src="https://cdn.iconscout.com/icon/free/png-256/pause-1809817-1535940.png"
-      className="w-12 h-12 cursor-pointer"
-      onClick={onClick}
-    />
-  ) : (
-    <img
-      src="https://icons-for-free.com/iconfiles/png/512/play+icon-1320184953284378621.png"
-      className="w-12 h-12 cursor-pointer"
-      onClick={onClick}
-    />
-  )
+// const PlayPause = ({ isPlaying, onClick }) =>
+//   isPlaying ? (
+//     <img
+//       src="https://cdn.iconscout.com/icon/free/png-256/pause-1809817-1535940.png"
+//       className="w-12 h-12 cursor-pointer"
+//       onClick={onClick}
+//     />
+//   ) : (
+//     <img
+//       src="https://icons-for-free.com/iconfiles/png/512/play+icon-1320184953284378621.png"
+//       className="w-12 h-12 cursor-pointer"
+//       onClick={onClick}
+//     />
+//   )
 
-const Podcast = ({
-  podcast,
-  isPlaying,
-  playingPodcastId,
-  setPlayingPodcastId,
-}) => {
-  return (
-    <div className="flex justify-between border-b pb-8 mb-8 max-w-3xl min-w-3xl">
-      <div className="flex">
-        <img src={podcast.image} className="w-40 h-40" />
-        <div className="flex flex-col ml-4 w-3/5">
-          <div className="text-lg font-semibold text-gray-900">
-            {podcast.title}
-          </div>
-          <div className="text-sm text-gray-800 mt-2">
-            {podcast.description}
-          </div>
-        </div>
-      </div>
-      <PlayPause
-        isPlaying={isPlaying}
-        onClick={() =>
-          isPlaying && playingPodcastId === podcast.id
-            ? setPlayingPodcastId(null)
-            : setPlayingPodcastId(podcast.id)
-        }
-      />
-    </div>
-  )
-}
+// const Podcast = ({
+//   podcast,
+//   isPlaying,
+//   playingPodcastId,
+//   setPlayingPodcastId,
+// }) => {
+//   return (
+//     <div className="flex justify-between border-b pb-8 mb-8 max-w-3xl min-w-3xl">
+//       <div className="flex">
+//         <img src={podcast.image} className="w-40 h-40" />
+//         <div className="flex flex-col ml-4 w-3/5">
+//           <div className="text-lg font-semibold text-gray-900">
+//             {podcast.title}
+//           </div>
+//           <div className="text-sm text-gray-800 mt-2">
+//             {podcast.description}
+//           </div>
+//         </div>
+//       </div>
+//       <PlayPause
+//         isPlaying={isPlaying}
+//         onClick={() =>
+//           isPlaying && playingPodcastId === podcast.id
+//             ? setPlayingPodcastId(null)
+//             : setPlayingPodcastId(podcast.id)
+//         }
+//       />
+//     </div>
+//   )
+// }
 
-export default function PodcastsPage({ data }) {
-  console.log('data')
-  console.log(data)
-  console.log('data.edges')
-  console.log(data.allMarkdownRemark.nodes)
+// export default function PodcastsPage({ data }) {
+//   console.log('data')
+//   console.log(data)
+//   console.log('data.edges')
+//   console.log(data.allMarkdownRemark.nodes)
 
-  const podcasts = data.allMarkdownRemark.nodes
+//   const podcasts = data.allMarkdownRemark.nodes
 
-  const [playingPodcastId, setPlayingPodcastId] = useState(null)
-  const [playingPodcastAudio, setPlayingPodcastAudio] = useState(null)
+//   const [playingPodcastId, setPlayingPodcastId] = useState(null)
+//   const [playingPodcastAudio, setPlayingPodcastAudio] = useState(null)
 
-  useEffect(() => {
-    if (playingPodcastAudio && playingPodcastId === null) {
-      playingPodcastAudio.pause()
-    }
-  }, [playingPodcastId])
+//   useEffect(() => {
+//     if (playingPodcastAudio && playingPodcastId === null) {
+//       playingPodcastAudio.pause()
+//     }
+//   }, [playingPodcastId])
 
-  useEffect(() => {
-    if (playingPodcastAudio) {
-      playingPodcastAudio.pause()
-    }
+//   useEffect(() => {
+//     if (playingPodcastAudio) {
+//       playingPodcastAudio.pause()
+//     }
 
-    if (playingPodcastId) {
-      console.log(playingPodcastId)
-      console.log(podcasts.find(podcast => podcast.id === playingPodcastId))
-      var sound = new Howl({
-        src: [podcasts.find(podcast => podcast.id === playingPodcastId).audio],
-      })
+//     if (playingPodcastId) {
+//       console.log(playingPodcastId)
+//       console.log(podcasts.find(podcast => podcast.id === playingPodcastId))
+//       var sound = new Howl({
+//         src: [podcasts.find(podcast => podcast.id === playingPodcastId).audio],
+//       })
 
-      sound.play()
-      setPlayingPodcastAudio(sound)
+//       sound.play()
+//       setPlayingPodcastAudio(sound)
 
-      console.log('ye')
-    }
-  }, [playingPodcastId])
+//       console.log('ye')
+//     }
+//   }, [playingPodcastId])
 
-  return (
-    <Layout>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Underguard Teleservices | Blog</title>
-        <link rel="canonical" href="https://ugteleservices.com/blog"></link>
-      </Helmet>
-      <section
-        className="section flex flex-col items-center mb-24"
-        style={{ minHeight: '90vh', paddingTop: 80 }}
-      >
-        <h1 className="text-gray-900 font-bold text-3xl mb-20 mt-20">
-          Podcasts
-        </h1>
-        <div className="w-full flex justify-center">
-          <div className="w-5/6 flex flex-col items-center">
-            {podcasts.map(podcast => (
-              <Podcast
-                podcast={podcast}
-                key={podcast.id}
-                isPlaying={playingPodcastId === podcast.id}
-                playingPodcastId={playingPodcastId}
-                setPlayingPodcastId={setPlayingPodcastId}
-              />
-            ))}
-          </div>
-        </div>
-      </section>
-    </Layout>
-  )
-}
+//   return (
+//     <Layout>
+//       <Helmet>
+//         <meta charSet="utf-8" />
+//         <title>Underguard Teleservices | Blog</title>
+//         <link rel="canonical" href="https://ugteleservices.com/blog"></link>
+//       </Helmet>
+//       <section
+//         className="section flex flex-col items-center mb-24"
+//         style={{ minHeight: '90vh', paddingTop: 80 }}
+//       >
+//         <h1 className="text-gray-900 font-bold text-3xl mb-20 mt-20">
+//           Podcasts
+//         </h1>
+//         <div className="w-full flex justify-center">
+//           <div className="w-5/6 flex flex-col items-center">
+//             {podcasts.map(podcast => (
+//               <Podcast
+//                 podcast={podcast}
+//                 key={podcast.id}
+//                 isPlaying={playingPodcastId === podcast.id}
+//                 playingPodcastId={playingPodcastId}
+//                 setPlayingPodcastId={setPlayingPodcastId}
+//               />
+//             ))}
+//           </div>
+//         </div>
+//       </section>
+//     </Layout>
+//   )
+// }
 
-export const pageQuery = graphql`
-  query PodcastQuery {
-    allMarkdownRemark(
-      filter: { frontmatter: { templateKey: { eq: "podcast" } } }
-    ) {
-      nodes {
-        id
-        fields {
-          slug
-        }
-        frontmatter {
-          date
-          author
-          title
-          description
-          audio
-        }
-      }
-    }
-  }
-`
+// export const pageQuery = graphql`
+//   query PodcastQuery {
+//     allMarkdownRemark(
+//       filter: { frontmatter: { templateKey: { eq: "podcast" } } }
+//     ) {
+//       nodes {
+//         id
+//         fields {
+//           slug
+//         }
+//         frontmatter {
+//           date
+//           author
+//           title
+//           description
+//           audio
+//         }
+//       }
+//     }
+//   }
+// `
 
 // const podcasts = [
 //   {
--- src/templates/podcast.js
+++ src/templates/podcast.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import { graphql } from 'gatsby'
+// import React from 'react'
+// import { graphql } from 'gatsby'
 
-export default function PodcastTemplate({ title, slug }) {
-  return <div>podcast: {title}</div>
-}
+// export default function PodcastTemplate({ title, slug }) {
+//   return <div>podcast: {title}</div>
+// }
 
-export const pageQuery = graphql`
-  query PodcastByID($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      id
-      fields {
-        slug
-      }
-      frontmatter {
-        title
-      }
-    }
-  }
-`
+// export const pageQuery = graphql`
+//   query PodcastByID($id: String!) {
+//     markdownRemark(id: { eq: $id }) {
+//       id
+//       fields {
+//         slug
+//       }
+//       frontmatter {
+//         title
+//       }
+//     }
+//   }
+// `
